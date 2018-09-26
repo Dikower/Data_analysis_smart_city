9f57d70834df0c619e849d930a2ada4834a8dc97
Dikower <dmitriy1d01@gmail.com>
2018-09-26 18:44:10 +0000

    Add new file

---
+++ Data preprocessing/raw_flats_to_prices_converter.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# =========================================================================================
+# Data from:
+# https://github.com/npetrenko/CIAN-parser
+# =========================================================================================
+
+import pandas as pd
+
+df = pd.read_csv("raw_flats.csv", encoding="utf8")
+df = df[df.roomarea5 <= 0]
+new_df = pd.DataFrame()
+
+for column in ["lat", "lon", "price", "overallarea"]:
+    new_df[column] = df[column]
+
+del df
+new_df["price_per_m"] = new_df["price"] / new_df["overallarea"]
+new_df["coors"] = new_df["lon"].astype(str)+','+new_df["lat"].astype(str)
+new_df = new_df.drop(["overallarea", "price", "lon", "lat"], axis=1)
+new_df.to_csv("prices.csv", index=False)
