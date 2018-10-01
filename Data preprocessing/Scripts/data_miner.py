c6cbd711ada0faaf5fff8dbf6693fc3fecaea477
Dikower <dmitriy1d01@gmail.com>
2018-10-01 21:54:42 +0300

    Update data_miner.py

--- Data preprocessing/Scripts/data_miner.py
+++ Data preprocessing/Scripts/data_miner.py
@@ -4,6 +4,7 @@
 # ==============================================================
 # author - Dikower (Din Dmitriy)
 # Here is the class for data mining and training model functions
+# Special thanks to Elena Nikitina for the code on BeatifulSoup
 # ==============================================================
 
 import os
@@ -13,12 +14,12 @@ import math
 import asyncio
 import aiohttp
 import socket
-
+from bs4 import BeautifulSoup
 import pandas as pd
 import numpy as np
 
 import logging
-logging.basicConfig(format='[%(asctime)s][%(levelname)s]-%(message)s',
+logging.basicConfig(format="[%(asctime)s][%(levelname)s]-%(message)s",
                     level=logging.INFO)
 logger = logging.getLogger(__name__)
 
@@ -41,12 +42,21 @@ class DataMiner:
         self.data_base = pd.read_csv(csv_file_path, sep=';', encoding="utf8")
         self.variable_data_base = None
         self.mine_coors = mine_coors
+        zero_vector = np.zeros(self.data_base.shape[0])
+        for _class in self.classes:
+            self.data_base[_class] = pd.Series(zero_vector)
+            self.data_base[f"min_distance_for_{_class}"] = pd.Series(zero_vector)
+            self.data_base[f"mean_distance_for_{_class}"] = pd.Series(zero_vector)
 
         self.metro_data_path = metro_data_path
         try:
             os.mkdir("backups")
         except FileExistsError:
             pass
+
+        logging.basicConfig(format="[%(asctime)s][%(levelname)s] - %(message)s",
+                            level=logging.INFO)
+        self.logger = logging.getLogger(__name__)
 
     # The function which starts coroutines filling data_base with coordinates
     async def get_coors(self, session):
@@ -60,7 +70,8 @@ class DataMiner:
         self.data_base["coors"] = pd.Series(coors)
 
     # The function which starts coroutines adding classes columns
-    async def search_by_column(self, session):  # makes n parallel responses for n flats
+    async def search_by_columns(self, session):  # makes n parallel responses for n flats
+        self.logger.info("Starting search by columns")
         for _class in self.classes:
             # using index because of async returning => non sorted results
             futures = [asyncio.ensure_future(self.search_objects_class(session, index, coordinates, _class))
@@ -90,12 +101,7 @@ class DataMiner:
 
     # The function which starts coroutines adding filled rows
     async def search_by_rows(self, session):  # makes n parallel requests for n classes
-        zero_vector = np.zeros(self.data_base.shape[0])
-        for _class in self.classes:
-            self.data_base[_class] = pd.Series(zero_vector)
-            self.data_base[f"min_distance_for_{_class}"] = pd.Series(zero_vector)
-            self.data_base[f"mean_distance_for_{_class}"] = pd.Series(zero_vector)
-
+        self.logger.info("Starting search by rows")
         for index, flat in self.data_base.iterrows():
             # using index because of async returning => non sorted results
             futures = [asyncio.ensure_future(self.search_objects_class(session, index, flat["coors"], _class))
@@ -110,14 +116,18 @@ class DataMiner:
 
             if (index + 1) % 100 == 0:
                 self.data_base.to_csv(f"backups/backup_{time.time()}.csv", sep=";", encoding="utf8", index=False)
-                logger.info(f"{round(index/self.data_base.shape[0] * 100, 2)}%")
+                self.logger.info(f"{round(index/self.data_base.shape[0] * 100, 2)}%")
 
     # The request coroutine getting coordinates from address using yandex geocode
     async def find_object(self, session, index, address):
+        toponym_coodrinates = ""
         async with session.get(self.geocode_url, params={"geocode": address, "format": self.format}) as response:
             json_response = await response.json()
-        toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
-        toponym_coodrinates = toponym["Point"]["pos"]
+            try:
+                toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
+                toponym_coodrinates = toponym["Point"]["pos"]
+            except:
+                self.logger.info(f"ERROR in find_object! Geocoder response: {json_response}")
         # print(toponym_coodrinates)
         return index, toponym_coodrinates.split()
 
@@ -136,26 +146,26 @@ class DataMiner:
 
         async with session.get(self.search_url, params=search_params) as response:
             json_response = await response.json()
-            # logger.info(json_response)
+            # self.logger.info(json_response)
             try:
                 value = len(json_response["features"])
                 objects = {}
-                for object in json_response["features"]:
-                    name = object["properties"]["CompanyMetaData"]["name"]
+                for _object in json_response["features"]:
+                    name = _object["properties"]["CompanyMetaData"]["name"]
                     if objects_class == "метро":
                         if name in self.variable_data_base.columns:
                             self.data_base["prices_near_metro"] = self.variable_data_base[name]
-                    object_coordinates = object['geometry']['coordinates']
+                    object_coordinates = _object['geometry']['coordinates']
                     distance = await self.distance(coordinates.split(","), object_coordinates)
                     objects[name] = distance
 
                 min_distance = 0
                 mean_distance = 0
                 if objects != {}:
-                    min_distance = objects[min(objects.keys(), key=lambda x: objects[x])]
-                    mean_distance = np.array(list(objects.values())).mean()
+                    min_distance = round(objects[min(objects.keys(), key=lambda x: objects[x])])
+                    mean_distance = round(np.array(list(objects.values())).mean())
             except:
-                logger.info("ERROR!")
+                self.logger.info(f"ERROR in search! Organisation search response: {json_response}")
                 index, value, min_distance, mean_distance = 0, 0, 0, 0
 
             return objects_class, index, value, min_distance, mean_distance
@@ -163,6 +173,32 @@ class DataMiner:
     def add_metro_data(self):
         self.variable_data_base = pd.read_csv(self.metro_data_path, encoding="utf8", sep=";")
         self.data_base["prices_near_metro"] = pd.Series(np.zeros(self.data_base.shape[0]))
+
+    @staticmethod
+    async def parse_flat_page(session, url):
+        evaluations = {
+            "Конструктив и состояние": 0,
+            "Положительное соседство": 0,
+            "Отрицательное соседство": 0,
+            "Квартиры и планировки": 0,
+            "Инфраструктура": 0,
+            "Безопасность": 0,
+            "Транспорт": 0,
+            "Экология": 0
+        }
+        async with session.get(url) as response:
+            bs = BeautifulSoup(await response.text(), "lxml")
+            spans = bs.find_all("span")
+            for span in spans:
+                if "class" in span.attrs:
+                    for sp in span["class"]:
+                        evaluation_class = span.parent.parent.parent.text
+                        if "star_full" in sp:
+                            evaluations[evaluation_class] = evaluations.get(evaluation_class, 0) + 1
+                        elif "star_half" in sp:
+                            evaluations[evaluation_class] = evaluations.get(evaluation_class, 0) + 0.5
+
+            return evaluations
 
     # The function calculating distance between two dots
     @staticmethod
@@ -194,7 +230,7 @@ class DataMiner:
             if rows:
                 await self.search_by_rows(session)
             else:
-                await self.search_by_column(session)
+                await self.search_by_columns(session)
 
 
 # Buildings type to search (features for model)
@@ -204,12 +240,13 @@ classes = ["метро", "аптека", "парк", "кафе", "торговы
 # Language of classes (find supporting languages on https://tech.yandex.ru/maps/geosearch/?from=mapsapi)
 language = "ru_RU"
 
-# Your token for yandex geocoder api
-token = ""
+# Your token for yandex organisation search api
+# This one allows you to make only 500 requests, so you can fill only 33 rows
+token = "3c4a592e-c4c0-4949-85d1-97291c87825c"  #
 
-# File path (the class opens csv with sep=';'. The file columns: address; ... your columns for model;)
-path = "Data/prices.csv"
-metro_data_path = "Data/processed_prices_near_metro"
+# File path (the class opens csv with sep=';'. The file columns: address; ... your columns for model)
+path = "prices.csv"
+metro_data_path = "processed_prices_near_metro"
 # If there is column with coors switch to False
 mine_coors = False
 # Not recommended to switch to False
@@ -219,6 +256,7 @@ mine_by_rows = True
 # Starts mining
 dm = DataMiner(token, path, metro_data_path, classes, language, mine_coors)
 dm.event_loop.run_until_complete(dm.mine(mine_by_rows))
+dm.add_metro_data()
 dm.event_loop.close()
 
 # Saves mined data to csv
